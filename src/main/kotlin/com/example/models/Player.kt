ddad90694c5048ac1d3d0cd31015a0082e4baa1b
sanjeev113f <sanjeevk@sahaj.ai>
2023-03-06 16:16:42 +0530

    Add formatting

--- src/main/kotlin/com/example/models/Player.kt
+++ src/main/kotlin/com/example/models/Player.kt
@@ -5,29 +5,22 @@ class Player(val person: Person) {
     private var foulCount = MIN_VALUE
     private var threeSuccessiveTurnPoint = MIN_VALUE
 
-    fun getGameScore(): Int {
-        return gameScore
+    fun getGameScore() = gameScore
+
+    fun getFoulCount() = foulCount
+
+    fun getThreeSuccessiveTurnEarning() = threeSuccessiveTurnPoint
+
+    fun updateFoulCount(value: Int) {
+        foulCount += value
     }
 
     fun updateGameScore(value: Int) {
         gameScore += value
     }
 
-    fun getFoulCount(): Int {
-        return foulCount
-    }
-
-    fun updateFoulCount(value: Int) {
-        foulCount += value
-    }
-
-    fun getThreeSuccessiveTurnEarning(): Int {
-        return threeSuccessiveTurnPoint
-    }
-
     fun updateSuccessiveTurnEarning(value: Int) {
         threeSuccessiveTurnPoint += value
     }
-
 
 }
